41dfcedacbbdd65c22f8141b73cf2932bafb3f33
naakhurz <157807981+naakhurz@users.noreply.github.com>
2024-06-11 23:43:29 -0300

    Update index.jsx

--- src/components/Pie/index.jsx
+++ src/components/Pie/index.jsx
@@ -47,7 +47,7 @@ function Pie() {
                 	</a>
             	</li>
         	</IconoContainer>
-        	<PieTexto>Desarrollado por Alura.</PieTexto>
+        	<PieTexto>Desarrollado por Maximiliano rios.</PieTexto>
     	</PieEstilizado>
 	);
 }
